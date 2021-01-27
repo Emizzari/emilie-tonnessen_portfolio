8dbba7a14017fb7d46f130669e0ce244b4b7b816
Emizzari <emilie@tonnessen.com>
2021-01-27 10:23:50 +0100

    semantic code

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,5 @@
 import mobileMenu from "./components/menu/mobileMenu.js"
-import { formValidation} from "./components/form/formValidation.js";
+import { formValidation } from "./components/form/formValidation.js";
 import { form } from "./components/form/formVariables.js";
 
 form.addEventListener("submit", formValidation);
