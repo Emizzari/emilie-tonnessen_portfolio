f9e3795915176dfe9745c41ded591322f8ff2e23
Emizzari <emilie@tonnessen.com>
2021-02-15 16:47:52 +0100

    Working projects

--- js/project.js
+++ js/project.js
@@ -1,9 +1,4 @@
-import { projectDetailsURL } from "./components/html/createProjectDetails.js";
-import { fetchAPI } from "./components/api/fetch.js";
-import { creatProjectDetails } from "./components/html/createProjectDetails.js";
 import mobileMenu from "./components/menu/mobileMenu.js";
 import { menuIcon } from "./components/abstracts/variables.js";
 
 menuIcon.addEventListener("click", mobileMenu);
-
-//fetchAPI(creatProjectDetails, projectDetailsURL);
